f8239493dfefdbb9365f360909e6747440468cf2
Aaron Groch <ar.groch@gmail.com>
2016-10-04 11:27:53 -0400

    in-class work, authentication and assocation

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :blog_posts
+
+  root 'blog_posts#index'
+
+  post 'create_comment' => 'blog_posts#create_comment'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
